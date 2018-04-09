87223dd3b4c9a2889e7bda72656dd95c0f668b3d
liuchao0704 <2820501417@qq.com>
2018-04-09 11:21:37 +0800

    angular-animation3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay'
+import { trigger, state, transition,style, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('square', [
+      state('green', style({ 'background-color': 'green' })),
+      state('red', style({ 'background-color': 'red' })),
+      transition('green => red', animate(1000)),
+      transition('red => green', animate(1000)),
+    ])
+  ]
 })
 export class AppComponent {
+  squareState : string;
   darkTheme = false;
+  constructor(private oc: OverlayContainer) {
+
+  }
   switchThems(dark) {
     this.darkTheme = dark;
+    this.oc.getContainerElement().classList.add('myapp-dark-theme');
+  }
+  onClick() {
+    this.squareState = this.squareState === 'red'? 'green': 'red';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatSidenavModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'
 import { CoreModule } from './core/core.module';
-import { LoginModule } from './login/login.module'; 
+import { LoginModule } from './login/login.module';
 import { ProjectModule } from './project/project.module';
 import { TaskModule } from './task/task.module';
 
@@ -20,6 +21,7 @@ import { TaskModule } from './task/task.module';
     LoginModule,
     ProjectModule,
     TaskModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
